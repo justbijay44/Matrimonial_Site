181dafbb8146e3b6e584d4e6ffe29337346b04cb
Bijay Syangtan <justbijay44@gmail.com>
2025-04-23 07:38:38 +0545

    More Addition CHAN

--- marital_website/settings.py
+++ marital_website/settings.py
@@ -52,8 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE.append('django_browser_reload.middleware.BrowserReloadMiddleware')
 
 ROOT_URLCONF = 'marital_website.urls'
 
--- marital_website/urls.py
+++ marital_website/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,10 @@ from marital.urls import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('marital.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
